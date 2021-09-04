ef9529b2ed05eaf1be0e02cca850cf50f623e8d7
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2021-09-04 06:56:19 +0530

    Sepearation of concern using modules

---
+++ WorkAround_Explorer/scripts/workAround.mjs
@@ -0,0 +1,30 @@
+import salaryData, { getDataByRole, getDataByCompany } from './salaryData.mjs';
+
+export const getAverageSalaryByRole = (role) => {
+  const roleData = getDataByRole(role);
+  const salariesOfRole = roleData.map((obj) => obj.salary);
+  return calculateAverage(salariesOfRole);
+};
+
+export const getAverageSalaryByCompany = (company) => {
+  const companyData = getDataByCompany(company);
+  const salariesAtCompany = companyData.map((obj) => obj.salary);
+  return calculateAverage(salariesAtCompany);
+};
+
+export const getSalaryAtCompany = (role, company) => {
+  const companyData = getDataByCompany(company);
+  const roleAtCompany = companyData.find((obj) => obj.role === role);
+  return roleAtCompany.salary;
+};
+
+export const getIndustryAverageSalary = () => {
+  const allSalaries = salaryData.map((obj) => obj.salary);
+  return calculateAverage(allSalaries);
+};
+
+function calculateAverage(arrayOfNumbers) {
+  let total = 0;
+  arrayOfNumbers.forEach((number) => (total += number));
+  return Number((total / arrayOfNumbers.length).toFixed(2));
+}
